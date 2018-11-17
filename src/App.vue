2c385119218619387950b1f87a547646ab1c3e4f
Adam Simonini <adamsimonini@users.noreply.github.com>
2018-11-17 14:54:28 -0500

    Vue3.0 Implementation Started

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Welcome msg="Welcome to Catalist"/>
+  </div>
+</template>
+
+<script>
+import Welcome from './components/Welcome.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Welcome
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="login-field">
+    <h2>Login Here</h2>
+    <form id="login-form">
+        <label for="email">E-mail:</label>
+        <input id="email-input" type="text"/>
+        <br />
+        <label for="password">Password:</label>
+        <input id="password-input" type="text"/>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #login-field{
+      text-align: center;  
+    }
+    #login-form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
