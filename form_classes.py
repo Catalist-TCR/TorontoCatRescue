6359cf48b4401285e7ac91e41d21fe15a5dfc244
JaneIllario <illariojane@gmail.com>
2018-11-17 12:25:31 -0500

    form changes

--- form_classes.py
+++ form_classes.py
@@ -12,36 +12,100 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-class CatInformation(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-    documents = FileField(validators=[FileRequired()])
+class ShelterCatInformation(FlaskForm):
+	date = DateField('Date', format='%Y-%m-%d')
+    name = StringField('Cat Name', validators=[DataRequired()])
+    date_of_birth = DateField('Date of Birth', format='%Y-%m-%d')
+    age = StringField('Age')
+    sex = SelectField('Sex', choices=['M','F'], validators=[Required()])
+    description = TextAreaField('Description')
+    sn = SelectField('S/N', choices=['Yes','No'], validators=[Required()])
+    shelter_name = StringField('Shelter Name', validators=[DataRequired()])
+    shelter_id = StringField('Shelter ID', validators=[DataRequired()])
+    photo = FileField('Cat Photo', validators=[FileRequired()])
+    fiv_tested = SelectField('FIV Tested', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    flv_tested = SelectField('FLV Tested', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    fvrcp_vaccination_date = DateField('FVRCP Vaccination Date', format='%Y-%m-%d')
+    rabies_vaccination_date = DateField('Rabies Vaccination Date', format='%Y-%m-%d')
+    medical_notes = TextAreaField('Medical Notes')
+    medical_documents = FileField('Medical Documents', validators=[FileRequired()])
+    behaviour_notes = TextAreaField('Behaviour Notes')
+    submit = SubmitField('Submit')
 
+class IntakeCatInformation(FlaskForm):
+	date = DateField('Date', format='%Y-%m-%d')
+    name = StringField('Cat Name', validators=[DataRequired()])
+    date_of_birth = DateField('Date of Birth', format='%Y-%m-%d')
+    age = StringField('Age')
+    sex = SelectField('Sex', choices=['M','F'], validators=[Required()])
+    description = TextAreaField('Description')
+    sn = SelectField('S/N', choices=['Yes','No'], validators=[Required()])
+    shelter_name = StringField('Shelter Name', validators=[DataRequired()])
+    shelter_id = StringField('Shelter ID', validators=[DataRequired()])
+    photo = FileField('Cat Photo', validators=[FileRequired()])
+    fiv_tested = SelectField('FIV Tested', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    flv_tested = SelectField('FLV Tested', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    fvrcp_vaccination_date = DateField('FVRCP Vaccination Date', format='%Y-%m-%d')
+    rabies_vaccination_date = DateField('Rabies Vaccination Date', format='%Y-%m-%d')
+    medical_notes = TextAreaField('Medical Notes')
+    medical_documents = FileField('Medical Documents', validators=[FileRequired()])
+    behaviour_notes = TextAreaField('Behaviour Notes')
+    #INTAKE
+    petpoint_id = StringField('Petpoint ID', validators=[DataRequired()])
+    outcome = TextAreaField('Entered into TCR program, euthanized, etc.')
+    intake_date = DateField('Intake Date', format='%Y-%m-%d')
+    submit = SubmitField('Submit')
+
+class FosterPlacementCatInformation(FlaskForm):
+	date = DateField('Date', format='%Y-%m-%d')
+    name = StringField('Cat Name', validators=[DataRequired()])
+    date_of_birth = DateField('Date of Birth', format='%Y-%m-%d')
+    age = StringField('Age')
+    sex = SelectField('Sex', choices=['M','F'], validators=[Required()])
+    description = TextAreaField('Description')
+    sn = SelectField('S/N', choices=['Yes','No'], validators=[Required()])
+    shelter_name = StringField('Shelter Name', validators=[DataRequired()])
+    shelter_id = StringField('Shelter ID', validators=[DataRequired()])
+    photo = FileField('Cat Photo', validators=[FileRequired()])
+    fiv_tested = SelectField('FIV Tested', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    flv_tested = SelectField('FLV Tested', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    fvrcp_vaccination_date = DateField('FVRCP Vaccination Date', format='%Y-%m-%d')
+    rabies_vaccination_date = DateField('Rabies Vaccination Date', format='%Y-%m-%d')
+    medical_notes = TextAreaField('Medical Notes')
+    medical_documents = FileField('Medical Documents', validators=[FileRequired()])
+    behaviour_notes = TextAreaField('Behaviour Notes')
+    petpoint_id = StringField('Petpoint ID', validators=[DataRequired()])
+    outcome = TextAreaField('Entered into TCR program, euthanized, etc.')
+    intake_date = DateField('Intake Date', format='%Y-%m-%d')
+    #FOSTER HOME
+    foster_placement_date = DateField('Placement Date', format='%Y-%m-%d')
+    location = SelectField('Location', choices=['Tested Positive', 'Tested Negative', 'Not Tested'], validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 '''
 
-Proposal Date
-Name
-Age
-Sex
-Description
-S/N
-Shelter Name
-Shelter ID
-Petpoint ID (Unique Identifier)
-Image
-FIV Tested (+/-) - Tested Positive, Tested Negative, Not Tested (default)
-FLV Tested (+/-) - Tested Positive, Tested Negative, Not Tested (default)
-FVRCP Vaccination Date
-Rabies Vaccination Date
-Medcial Notes
-Behaviour Notes
-Outcome - Entered into TCR program, euthanized, etc.
-Intake Date
-Foster Placement Date
-Location - Dropdown
+xProposal Date
+xName
+XDoB
+xAge
+xSex
+xDescription
+xS/N
+xShelter Name
+xShelter ID
+-----Petpoint ID (Unique Identifier)
+xImage
+xFIV Tested (+/-) - Tested Positive, Tested Negative, Not Tested (default)
+xFLV Tested (+/-) - Tested Positive, Tested Negative, Not Tested (default)
+xFVRCP Vaccination Date
+xRabies Vaccination Date
+xMedcial Notes
+xMedical Forms
+xBehaviour Notes
+-----Outcome - Entered into TCR program, euthanized, etc.
+-----Intake Date
+-----Foster Placement Date
+-----Location - Dropdown
 
 '''
