433e8f974ab775efab9089e9ff2d70d200859c09
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-11-17 10:02:48 -0500

    Adding original templating functions

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
-app = Flask(__name__)
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+	form = LoginForm()
+	return render_template('index.html', title='Sign In', form=form)
+
+def application():
+	STATIC_URL_PATH = '/static/'
+	app = Flask(__name__, static_url_path=STATIC_URL_PATH)
+	app.config['SECRET_KEY'] = 'cats'
+	app.run(debug=True)
