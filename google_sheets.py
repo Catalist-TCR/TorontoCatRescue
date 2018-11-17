35cc912bf3e38a55aa1f75290a3f45d8b25289cc
JaneIllario <illariojane@gmail.com>
2018-11-17 15:20:40 -0500

    debug main file save

--- google_sheets.py
+++ google_sheets.py
@@ -8,13 +8,13 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('google.auth', sc
 
 gc = gspread.authorize(credentials)
 
-#sh = gc.create('Catalist')
-#sh.share('christopher.paul.dryden@gmail.com', perm_type='user', role='writer')
+#sh = gc.create('users')
+#sh.share('illariojane@gmail.com', perm_type='user', role='writer')
 
 
 
-wks = gc.open("Catalist").sheet1
-row_list = wks.get_all_values())
+wks = gc.open("users").sheet1
+row_list = wks.get_all_values()
 print(len(row_list))
 print(wks)
 
